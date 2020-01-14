6a2aa5acc807697c7d2870258091bbeabe8bd1f8
Sasa Schwartz and Tiffany Siu <tiffany@>
2020-01-14 10:59:18 -0800

    build flashcards and toggle functionality

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+  $("#javascript").click(function(){
+    $("#javascript p").toggle();
+    $("#javascript h3").toggle();
+  });
+  $("#operators").click(function(){
+    $("#operators p").toggle();
+    $("#operators h3").toggle();
+  });
+  $("#variables").click(function(){
+    $("#variables p").toggle();
+    $("#variables h3").toggle();
+  });
+  $("#varNamingConventions").click(function(){
+    $("#varNamingConventions p").toggle();
+    $("#varNamingConventions h3").toggle();
+  });
+  $("#functions").click(function(){
+    $("#functions p").toggle();
+    $("#functions h3").toggle();
+  });
+  $("#methods").click(function(){
+    $("#methods p").toggle();
+    $("#methods h3").toggle();
+  });
+  $("#arguments").click(function(){
+    $("#arguments p").toggle();
+    $("#arguments h3").toggle();
+  });
+  $("#parameters").click(function(){
+    $("#parameters p").toggle();
+    $("#parameters h3").toggle();
+  });
+  $("#return").click(function(){
+    $("#return p").toggle();
+    $("#return h3").toggle();
+  });
+  $("#chainingMethods").click(function(){
+    $("#chainingMethods p").toggle();
+    $("#chainingMethods h3").toggle();
+  });
+  $("#strings").click(function(){
+    $("#strings p").toggle();
+    $("#strings h3").toggle();
+  });
+  $("#booleans").click(function(){
+    $("#booleans p").toggle();
+    $("#booleans h3").toggle();
+  });
+  $("#undefined").click(function(){
+    $("#undefined p").toggle();
+    $("#undefined h3").toggle();
+  });
+  $("#nan").click(function(){
+    $("#nan p").toggle();
+    $("#nan h3").toggle();
+  });
+  $("#escape").click(function(){
+    $("#escape p").toggle();
+    $("#escape h3").toggle();
+  });
+  $("#alert").click(function(){
+    $("#alert p").toggle();
+    $("#alert h3").toggle();
+  });
+  $("#comments").click(function(){
+    $("#comments p").toggle();
+    $("#comments h3").toggle();
+  });
+  $("#jQuery").click(function(){
+    $("#jQuery p").toggle();
+    $("#jQuery h3").toggle();
+  });
+  $("#attributes").click(function(){
+    $("#attributes p").toggle();
+    $("#attributes h3").toggle();
+  });
+});
